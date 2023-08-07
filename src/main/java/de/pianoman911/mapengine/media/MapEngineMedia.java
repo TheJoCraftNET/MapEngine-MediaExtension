2a3d7521c57242d41fd3386063c57623d0a8ccae
pianoman911 <pianofinn13@gmail.com>
2023-08-07 23:27:17 +0200

    Added updater

--- src/main/java/de/pianoman911/mapengine/media/MapEngineMedia.java
+++ src/main/java/de/pianoman911/mapengine/media/MapEngineMedia.java
@@ -1,6 +1,8 @@
 package de.pianoman911.mapengine.media;
 
+import de.pianoman911.mapengine.media.updater.MapEngineUpdater;
 import org.bstats.bukkit.Metrics;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MapEngineMedia extends JavaPlugin {
@@ -8,5 +10,15 @@ public class MapEngineMedia extends JavaPlugin {
     @Override
     public void onLoad() {
         new Metrics(this, 18145);
+    }
+
+    @Override
+    public void onEnable() {
+        this.saveDefaultConfig();
+
+        if (this.getConfig().getBoolean("updater.enabled", true)) {
+            MapEngineUpdater updater = new MapEngineUpdater(this);
+            Bukkit.getPluginManager().registerEvents(updater, this);
+        }
     }
 }
