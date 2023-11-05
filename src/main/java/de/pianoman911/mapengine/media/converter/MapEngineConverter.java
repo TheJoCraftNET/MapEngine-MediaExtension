37b7c6041414115bc4fe76c0e9afc2ad3ea74e99
pianoman911 <pianofinn13@gmail.com>
2023-11-05 13:53:26 +0100

    Improved map engine frame converter

--- src/main/java/de/pianoman911/mapengine/media/converter/MapEngineConverter.java
+++ src/main/java/de/pianoman911/mapengine/media/converter/MapEngineConverter.java
@@ -10,30 +10,35 @@ public class MapEngineConverter extends FrameConverter<FullSpacedColorBuffer> {
 
     @Override
     public Frame convert(FullSpacedColorBuffer bufferedImage) {
-        return null;
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public FullSpacedColorBuffer convert(Frame frame) {
-        if (frame.imageChannels != 3) {
-            throw new IllegalStateException("Frame must have 3 channels, we need RGBA. This frame has " + frame.imageChannels + " channels. Please contact me if you need support for this.");
+        if (frame.imageChannels != 3 && frame.imageChannels != 4) {
+            throw new IllegalStateException("Frame must have 3/4 channels, we need BGR/ABGR. This frame has " + frame.imageChannels + " channels. Please contact me if you need support for this.");
         }
-        FullSpacedColorBuffer result = new FullSpacedColorBuffer(frame.imageWidth, frame.imageHeight);
+
         ByteBuffer buffer = (ByteBuffer) frame.image[0].slice();
         if (buffer.remaining() == 0) {
             return null;
         }
-        int[] data = new int[buffer.remaining() / 3];
-        for (int i = 0; i < data.length; i++) {
-            int a = 255;
-            int b = buffer.get() & 0xFF;
-            int g = buffer.get() & 0xFF;
-            int r = buffer.get() & 0xFF;
 
-            data[i] = (a << 24) | (r << 16) | (g << 8) | b;
+        int[] data = new int[frame.imageWidth * frame.imageHeight];
+        boolean hasAlpha = frame.imageChannels == 4;
+        int index = 0;
+
+        for (int y = 0; y < frame.imageHeight; y++) {
+            for (int x = 0; x < frame.imageWidth; x++) {
+                int r = buffer.get(index) & 0xFF;
+                int g = buffer.get(index + 1) & 0xFF;
+                int b = buffer.get(index + 2) & 0xFF;
+                int a = hasAlpha ? buffer.get(index + 3) & 0xFF : 255;
+                data[y * frame.imageWidth + x] = (a << 24) | (r << 16) | (g << 8) | b;
+                index += frame.imageChannels;
+            }
+            index += frame.imageStride - frame.imageWidth * frame.imageChannels;
         }
-        System.arraycopy(data, 0, result.buffer(), 0, data.length);
-
-        return result;
+        return new FullSpacedColorBuffer(data, frame.imageWidth, frame.imageHeight);
     }
 }
